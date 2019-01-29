31653476a9cbffbe4c316386f3ee88068d1257ad
Glo Austin <glo.austin19@gmail.com>
2019-01-29 13:40:58 -0800

    made SideDrawer disappear when BackDrop is clicked in mobile view

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { Component } from "react";
 
-import Aux from '../../hoc/Aux';
-import classes from './Layout.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar'
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
+import Aux from "../../hoc/Aux";
+import classes from "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = ( props ) => (
-    <Aux>
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  };
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  render() {
+    return (
+      <Aux>
         <Toolbar />
         {/* changing the css dynamically using props */}
-        <SideDrawer height='80%' />
-        <div>
-            Toolbar, SideDrawer, Backdrop
-        </div>
+        <SideDrawer
+          height="80%"
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <div>Toolbar, SideDrawer, Backdrop</div>
 
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
 
-export default layout;
+export default Layout;
